8aeb516d495295318b8ef943790ecea07b036221
fedec <fedecvz@gmail.com>
2018-08-28 13:12:44 -0300

    Mount routes for authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #devise_for :users
+  mount_devise_token_auth_for 'User', at: '/'
 end
