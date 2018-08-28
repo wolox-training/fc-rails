3ae370b0e912f05881601200a90dadbc055f41e5
fedec <fedecvz@gmail.com>
2018-08-28 18:34:34 -0300

    Adds before action in api_controller.rb

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,4 +1,3 @@
 class Api::V1::ApiController < ApplicationController
-  include DeviseTokenAuth::Concerns::SetUserByToken
-  protect_from_forgery with: :null_session
+  before_action :authenticate_user!
 end
