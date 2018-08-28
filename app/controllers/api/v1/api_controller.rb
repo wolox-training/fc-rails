a181609ac1ed92abcf15715a393bd0c2902935d4
fedec <fedecvz@gmail.com>
2018-08-28 14:38:20 -0300

    Move configuration of devise token auth from application_controller.rb to api_controller.rb

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,2 +1,4 @@
 class Api::V1::ApiController < ApplicationController
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  protect_from_forgery with: :null_session
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,2 @@
 class ApplicationController < ActionController::Base
-  include DeviseTokenAuth::Concerns::SetUserByToken
-  protect_from_forgery with: :null_session
-
 end
