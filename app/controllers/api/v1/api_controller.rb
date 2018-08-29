16eafe0ed4137ee82cc7f74e1fbc2aae3301804c
fedec <fedecvz@gmail.com>
2018-08-29 09:10:32 -0300

    Nest modules and classes in ApiController

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,3 +1,7 @@
-class Api::V1::ApiController < Api::V1::BaseApiController
-  before_action :authenticate_api_v1_user!
+module Api
+  module V1
+    class ApiController < Api::V1::BaseApiController
+      before_action :authenticate_api_v1_user!
+    end
+  end
 end
