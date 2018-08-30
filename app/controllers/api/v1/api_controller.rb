72409937f63e2878bced54900e3be14bbd5821c1
fedec <fedecvz@gmail.com>
2018-08-30 17:36:14 -0300

    Unifies BaseApiController and ApiController

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,7 +1,10 @@
 module Api
   module V1
-    class ApiController < Api::V1::BaseApiController
+    class ApiController < ApplicationController
       before_action :authenticate_api_v1_user!
+      skip_before_action :authenticate_api_v1_user!
+      include DeviseTokenAuth::Concerns::SetUserByToken
+      protect_from_forgery with: :null_session
     end
   end
 end
