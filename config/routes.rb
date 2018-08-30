b7ac5ab456cc1184af00ff8e4926e281e5f12e19
fedec <fedecvz@gmail.com>
2018-08-30 11:12:55 -0300

    Routes book index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      get 'books', to: 'book#index'
+    end
+  end
+  namespace :api do
+    namespace :v1 do
       mount_devise_token_auth_for 'User', at: 'users/sessions'
     end
   end
