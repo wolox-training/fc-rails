56197b8cd92ab78eb93b2d9b166d61e80493f94e
fedec <fedecvz@gmail.com>
2018-08-31 12:46:00 -0300

    Enable Rails/LexicallyScopedActionFilter after filter method

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -4,6 +4,7 @@ module Api
       before_action :authenticate_api_v1_user!
       # rubocop:disable Rails/LexicallyScopedActionFilter
       skip_before_action :authenticate_api_v1_user!, only: [:create]
+      # rubocop:enable Rails/LexicallyScopedActionFilter
       include DeviseTokenAuth::Concerns::SetUserByToken
       protect_from_forgery with: :null_session
     end
