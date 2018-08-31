bd6ce8de7d56453ff8cc8f874fa7a78fc198d72f
fedec <fedecvz@gmail.com>
2018-08-31 12:39:04 -0300

    Disable cop in ApiController

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -2,6 +2,7 @@ module Api
   module V1
     class ApiController < ApplicationController
       before_action :authenticate_api_v1_user!
+      # rubocop:disable Rails/LexicallyScopedActionFilter
       skip_before_action :authenticate_api_v1_user!, only: [:create]
       include DeviseTokenAuth::Concerns::SetUserByToken
       protect_from_forgery with: :null_session
