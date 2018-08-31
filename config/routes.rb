3becee604e814f5e1d6eafb591c91f9b1badde8a
fedec <fedecvz@gmail.com>
2018-08-31 10:59:18 -0300

    Delete routing for session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    sessions: 'users/sessions'
-  }
-
   namespace :api do
     namespace :v1 do
       mount_devise_token_auth_for 'User', at: 'users/sessions'
