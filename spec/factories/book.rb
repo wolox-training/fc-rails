a7dcf8d1fd522be9d9e330ec855e50fe2b3cb0c3
fedec <fedecvz@gmail.com>
2018-09-03 16:38:18 -0300

    Create book.rb

---
+++ spec/factories/book.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+    factory :book do
+      genre 'Novela'
+      author 'Anthony Burgess'
+      image 'naranja_mecanica.jpg'
+      title 'La naranja mecanica'
+      publisher 'Editorial Sarlanga'
+      year '1970'
+    end
+end
