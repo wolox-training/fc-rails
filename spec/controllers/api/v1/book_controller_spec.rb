9f3729aee61577d9e0a31510475a8b6aadd8686e
fedec <fedecvz@gmail.com>
2018-09-04 09:56:34 -0300

    Add test for get index of book

--- spec/controllers/api/v1/book_controller_spec.rb
+++ spec/controllers/api/v1/book_controller_spec.rb
@@ -4,20 +4,40 @@ describe Api::V1::BookController, type: :controller do
   include_context 'Authenticated User'
 
   describe 'GET #show' do
-    context 'When fetching a user rent' do
+    context 'When fetching a book' do
       let!(:book) { create(:book) }
 
       before do
         get :show, params: { id: book.id }
       end
 
-      it 'responses with the user rent json' do
+      it 'responses with the book json' do
         expect(response_body.to_json).to eq BookSerializer.new(
           book, root: false
         ).to_json
       end
 
       it 'responds with 200 status' do
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  end
+
+  describe 'GET #index' do
+    context 'When fetching all the books' do
+      let!(:books) { FactoryBot.create_list(:book, 2) }
+      before do
+        get :index
+      end
+
+      it 'responses with the lists of books' do
+        expected = ActiveModel::Serializer::CollectionSerializer.new(
+          books, each_serializer: BookSerializer
+        ).to_json
+        expect(response_body['page'][0][1].to_json).to eq expected
+      end
+
+      it 'responses with 200 status' do
         expect(response).to have_http_status(:ok)
       end
     end
