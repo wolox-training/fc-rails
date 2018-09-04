470d5e68754a88c50597ad42708f221c1920ed4d
fedec <fedecvz@gmail.com>
2018-09-04 17:48:26 -0300

    Change book_controller.rb to books_controller.rb

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,6 +1,6 @@
 module Api
   module V1
-    class BookController < Api::V1::ApiController
+    class BooksController < Api::V1::ApiController
       def index
         @books = Book.all
         render json: @books
