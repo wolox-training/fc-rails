25a82709accb806132910fbaafcac8bcf1970f32
fedec <fedecvz@gmail.com>
2018-09-04 18:14:00 -0300

    Put the faker between brackets for book

--- spec/factories/book.rb
+++ spec/factories/book.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
     factory :book do
-      genre { Faker::Book.genre }
-      author { Faker::Book.author }
-      image { Faker::File.file_name() }
-      title { Faker::Book.title }
-      publisher { Faker::Book.publisher }
-      year { Faker::Number.number(4) }
+      genre (Faker::Book.genre)
+      author (Faker::Book.author)
+      image (Faker::File.file_name())
+      title (Faker::Book.title)
+      publisher (Faker::Book.publisher)
+      year (Faker::Number.number(4))
     end
 end
