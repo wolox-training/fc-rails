f73ae707c6a281ef1e85732033c03101e20a60c8
fedec <fedecvz@gmail.com>
2018-09-07 17:24:01 -0300

    Change directory of sessions_controller.rb

--- app/controllers/session/sessions_controller.rb
+++
@@ -1,6 +0,0 @@
-class Session::SessionsController < Devise::SessionsController
-  before_action :authenticate_api_v1_user!
-  # rubocop:disable Rails/LexicallyScopedActionFilter
-  skip_before_action :authenticate_api_v1_user!, only: [:create]
-  # rubocop:enable Rails/LexicallyScopedActionFilter
-end
