c3f4b97b3c4cf86f79845a60b92434bfb9ca236e
fedec <fedecvz@gmail.com>
2018-09-10 12:48:04 -0300

    Create route for create rent

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :books, only: [:show, :index]
-      resources :rents, only: [:create, :index], at: 'users/:user_id/rents' 
+      resources :rents, only: [:create, :index], path: 'users/:user_id/rents'
       mount_devise_token_auth_for 'User', at: 'users/sessions', controllers: { sessions: "api/v1/devise_token_auth_custom/sessions" }
     end
   end
