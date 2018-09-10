6ad2639d05f77feb6990d7411c1df7301f276fac
fedec <fedecvz@gmail.com>
2018-09-10 12:54:30 -0300

    Create create strong params for create rent

--- app/controllers/api/v1/rents_controller.rb
+++ app/controllers/api/v1/rents_controller.rb
@@ -2,18 +2,13 @@ module Api
   module V1
     class RentsController < ApiController
       def create
-        rent = Rent.create(
-          'user_id': params[:user_id],
-          'book_id': params[:book_id],
-          'from': params[:from],
-          'to': params[:to]
-        )
+        rent = Rent.create(rents_create_params)
         render json: rent
       end
 
       private
 
-      def rents_params
+      def rents_create_params
         params.permit(:user_id, :book_id, :from, :to)
       end
     end
