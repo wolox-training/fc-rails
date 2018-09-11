d586c8c22bcc32bd7519a3edaf3ae889bbecb723
fedec <fedecvz@gmail.com>
2018-09-11 15:29:05 -0300

    Fix rubocop offenses

--- app/controllers/api/v1/rents_controller.rb
+++ app/controllers/api/v1/rents_controller.rb
@@ -3,10 +3,7 @@ module Api
     class RentsController < ApiController
       def create
         rent = Rent.create(rents_create_params)
-
-        if rent.save
-          SendMailWorker.perform_async(rent.id)
-        end
+        SendMailWorker.perform_async(rent.id) if rent.save
 
         render json: rent
       end
