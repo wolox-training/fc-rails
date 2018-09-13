f27e634615cb02a2dafa7104738abac453d7ca49
fedec <fedecvz@gmail.com>
2018-09-13 15:57:08 -0300

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_12_175655) do
+ActiveRecord::Schema.define(version: 2018_09_13_182331) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -96,6 +96,7 @@ ActiveRecord::Schema.define(version: 2018_09_12_175655) do
     t.string "name"
     t.string "nickname"
     t.string "image"
+    t.string "locale", default: "en"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
