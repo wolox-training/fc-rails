a7dff9c45636c92803d0ea0368c11fc3c818f42b
fedec <fedecvz@gmail.com>
2018-09-14 16:41:09 -0300

    Fix rubocop offenses

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   include Wor::Paginate
-  rescue_from  ActiveRecord::RecordInvalid, with: :render_validation_error
+  rescue_from ActiveRecord: :RecordInvalid, with: :render_validation_error
 
   def render_validation_error
-    render json: {message: "Validation failed"}, status: :unprocessable_entity
+    render json: { message: 'Validation failed' }, status: :unprocessable_entity
   end
 end
