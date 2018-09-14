f015fca0c4fb82ff3d564d992a3746269ebe5ee2
fedec <fedecvz@gmail.com>
2018-09-14 18:06:16 -0300

    Create route for create of book suggestion

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :books, only: [:show, :index]
       resources :rents, only: [:create, :index], path: 'users/:user_id/rents'
+      resources :book_suggestions, only: [:create]
       mount_devise_token_auth_for 'User', at: 'users/sessions', controllers: { sessions: "api/v1/devise_token_auth_custom/sessions" }
     end
   end
