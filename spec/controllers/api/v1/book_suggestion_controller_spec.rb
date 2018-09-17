0ac608c102f31e156df229db65fac935ce4e521d
fedec <fedecvz@gmail.com>
2018-09-17 12:02:51 -0300

    Add tests to book_suggestion_controller_spec.rb

--- spec/controllers/api/v1/book_suggestion_controller_spec.rb
+++ spec/controllers/api/v1/book_suggestion_controller_spec.rb
@@ -6,37 +6,35 @@ describe Api::V1::BookSuggestionsController, type: :controller do
   describe 'POST #create' do
     context 'When creating a valid book suggestion' do
       let!(:new_book_suggestion_attributes) { FactoryBot.attributes_for(:book_suggestion, user: user).merge(user_id: user.id) }
+
       it 'creates a new book suggestion' do
         expect do
           post :create, params: new_book_suggestion_attributes
         end.to change { BookSuggestion.count }.by(1)
       end
 
-      # it 'responds with 201 status' do
-      #   post :create, params: { id: user.id, rent: new_book_suggestion_attributes }
-      #   expect(response).to have_http_status(:created)
-      # end
+      it 'responds with 200 status' do
+        post :create, params: new_book_suggestion_attributes
+        expect(response).to have_http_status(:ok)
+      end
     end
 
-    # context 'When creating an invalid user rent' do
-    #   let!(:new_book_suggestion_attributes) { attributes_with_foreign_keys(:rent, book: nil) }
-    #   before do
-    #     post :create, params: { id: user.id, rent: new_book_suggestion_attributes }
-    #   end
-    #
-    #   it 'doesn\'t create a new rent' do
-    #     expect do
-    #       post :create, params: { id: user.id, rent: new_book_suggestion_attributes }
-    #     end.to change { Rent.count }.by(0)
-    #   end
-    #
-    #   it 'returns error messages' do
-    #     expect(response_body['error']).to be_present
-    #   end
-    #
-    #   it 'responds with 422 status' do
-    #     expect(response).to have_http_status(:unprocessable_entity)
-    #   end
-    # end
+    context 'When creating an invalid book suggestion' do
+      let!(:new_book_suggestion_attributes) { FactoryBot.attributes_for(:book_suggestion) }
+
+      before do
+        post :create, params: new_book_suggestion_attributes
+      end
+
+      it 'doesn\'t create a new book suggestion' do
+        expect do
+          post :create, params: new_book_suggestion_attributes
+        end.to change { BookSuggestion.count }.by(0)
+      end
+
+      it 'responds with 422 status' do
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
   end
 end
