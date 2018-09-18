f2c50c905169479d56da315c8fe0a8a55f329d91
fedec <fedecvz@gmail.com>
2018-09-18 12:19:33 -0300

    Create methods new and create_without_auth on book suggestion controller

--- app/controllers/api/v1/book_suggestions_controller.rb
+++ app/controllers/api/v1/book_suggestions_controller.rb
@@ -1,7 +1,18 @@
 module Api
   module V1
     class BookSuggestionsController < ApiController
+      skip_before_action :authenticate_api_v1_user!, only: [:new, :create_without_auth]
+
       def create
+        book_suggestion = BookSuggestion.new(book_suggestions_create_params)
+        book_suggestion.save!
+        render json: book_suggestion
+      end
+
+      def new
+      end
+
+      def create_without_auth
         book_suggestion = BookSuggestion.new(book_suggestions_create_params)
         book_suggestion.save!
         render json: book_suggestion
