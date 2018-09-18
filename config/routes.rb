10614c79dc8c536152a0e1b61b92ad0dbdf3e593
fedec <fedecvz@gmail.com>
2018-09-18 12:20:07 -0300

    Create routes for get/post of new book suggestion

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :books, only: [:show, :index]
       resources :rents, only: [:create, :index], path: 'users/:user_id/rents'
-      resources :book_suggestions, only: [:create]
+      resources :book_suggestions, only: [:create, :new]
+      post '/book_suggestions/new', to: 'book_suggestions#create_without_auth'
       mount_devise_token_auth_for 'User', at: 'users/sessions', controllers: { sessions: "api/v1/devise_token_auth_custom/sessions" }
     end
   end
