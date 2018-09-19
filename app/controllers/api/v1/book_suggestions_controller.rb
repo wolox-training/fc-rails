a967c5bf867cbd89d6cdff253cbad648654c6601
fedec <fedecvz@gmail.com>
2018-09-19 17:37:30 -0300

    Make create of book suggestion not authenticated

--- app/controllers/api/v1/book_suggestions_controller.rb
+++ app/controllers/api/v1/book_suggestions_controller.rb
@@ -1,14 +1,12 @@
 module Api
   module V1
     class BookSuggestionsController < ApiController
-      skip_before_action :authenticate_api_v1_user!, only: %i[new create_without_auth]
+      skip_before_action :authenticate_api_v1_user!, only: %i[new create]
 
       def create
-        create_new_book_suggestion
-      end
-
-      def create_without_auth
-        create_new_book_suggestion
+        book_suggestion = BookSuggestion.new(book_suggestions_create_params)
+        book_suggestion.save!
+        render json: book_suggestion
       end
 
       def new; end
@@ -17,12 +15,6 @@ module Api
 
       def book_suggestions_create_params
         params.permit(:author, :title, :link, :editorial, :publisher, :year, :price, :user_id)
-      end
-
-      def create_new_book_suggestion
-        book_suggestion = BookSuggestion.new(book_suggestions_create_params)
-        book_suggestion.save!
-        render json: book_suggestion
       end
     end
   end
