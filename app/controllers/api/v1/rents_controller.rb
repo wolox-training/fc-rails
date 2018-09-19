98a95cc5cfc44f0218bd1f61c496ba5821ea0e08
fedec <fedecvz@gmail.com>
2018-09-19 12:36:59 -0300

    Delete duplicate code

--- app/controllers/api/v1/rents_controller.rb
+++ app/controllers/api/v1/rents_controller.rb
@@ -3,8 +3,7 @@ module Api
     class RentsController < ApiController
       def create
         rent = Rent.new(rents_create_params)
-        rent.save!
-        NotificationGeneratorMailer.with(rent: rent).new_rent_notification.deliver_later if rent.save
+        NotificationGeneratorMailer.with(rent: rent).new_rent_notification.deliver_later if rent.save!
         render json: rent
       end
 
