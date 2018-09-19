7c36502255d0970adefa37224192dcf8a0aba109
fedec <fedecvz@gmail.com>
2018-09-19 16:18:12 -0300

    Delete rescue_from sentence from application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,3 @@
 class ApplicationController < ActionController::Base
   include Wor::Paginate
-  rescue_from ActiveRecord::RecordInvalid, with: :render_validation_error
-
-  def render_validation_error
-    render json: { message: 'Validation failed' }, status: :unprocessable_entity
-  end
 end
