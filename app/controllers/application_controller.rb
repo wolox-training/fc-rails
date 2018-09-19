e4315a50160eb7cb56f5e3ad35cbfa38237f77ac
fedec <fedecvz@gmail.com>
2018-09-19 12:16:47 -0300

    Fix syntax error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   include Wor::Paginate
-  rescue_from ActiveRecord: :RecordInvalid, with: :render_validation_error
+  rescue_from ActiveRecord::RecordInvalid, with: :render_validation_error
 
   def render_validation_error
     render json: { message: 'Validation failed' }, status: :unprocessable_entity
