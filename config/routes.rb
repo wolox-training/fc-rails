30454255911c87ff99a135a20acfef40a95a4960
fedec <fedecvz@gmail.com>
2018-09-21 09:59:47 -0300

    Add route for onmiauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   namespace :api do
     namespace :v1 do
       resources :books, only: [:show, :index]
