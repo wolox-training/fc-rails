0a8477f972ed9a1535ff85542c4db93fde864c99
fedec <fedecvz@gmail.com>
2018-09-21 09:58:30 -0300

    Add method from_omniauth to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,21 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   include DeviseTokenAuth::Concerns::User
+
+  devise :omniauthable, omniauth_providers: [:google_oauth2]
+
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(email: data['email']).first
+
+    unless user
+    user = User.create(
+      name: data['name'],
+      email: data['email'],
+      password: Devise.friendly_token[0,20]
+    )
+    end
+
+    user
+  end
 end
