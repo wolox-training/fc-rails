433a06aaf6eaccbe4c1fd138dc1180968ee6c050
fedec <fedecvz@gmail.com>
2018-09-21 15:41:53 -0300

    Delete comments

--- app/services/open_library_service.rb
+++ app/services/open_library_service.rb
@@ -2,11 +2,5 @@ class OpenLibraryService
   include HTTParty
   def book_info(isbn)
     books = HTTParty.get('https://openlibrary.org/api/books?bibkeys=ISBN:' + isbn + '&format=json&jscmd=data')
-
-    ISBN
-Title
-Subtitle
-Number of pages
-Authors (Array con los nombres de los autores)
   end
 end
