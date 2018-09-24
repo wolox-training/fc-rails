04795bf176d6bcd230be31634c95c2ddb3ef057f
fedec <fedecvz@gmail.com>
2018-09-24 14:52:34 -0300

    Fix rubocop offenses

--- app/services/open_library_service.rb
+++ app/services/open_library_service.rb
@@ -2,9 +2,9 @@ class OpenLibraryService
   include HTTParty
   def book_info(isbn)
     query = {
-      :bibkeys => 'ISBN:' + isbn,
-      :format => 'json',
-      :jscmd => 'data'
+      bibkeys: 'ISBN:' + isbn,
+      format: 'json',
+      jscmd: 'data'
     }
     HTTParty.get(
       'https://openlibrary.org/api/books?',
